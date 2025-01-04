5e99de7fa95df458ff5af2cae54860ee80b8d9d7
Toshiya-Kato <toshiya.kato@linecorp.com>
2025-01-04 19:04:44 +0900

    Clean up functions

--- build.js
+++ build.js
@@ -8,17 +8,3 @@ esbuild.buildSync({
   sourcemap: true,
   target: ['esnext']
 });
-
-esbuild.buildSync({
-  entryPoints: ['src/functions/ogp.ts'],
-  outdir: 'functions',
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  target: ['esnext'],
-  format: 'esm',
-  loader: {
-    '.wasm': 'file'
-  },
-  assetNames: '[name]-[hash]',
-});
