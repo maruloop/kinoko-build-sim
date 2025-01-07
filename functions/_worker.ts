53ff9978bc11ed84e4900b29583cbd7f37e0c0a8
Toshiya-Kato <toshiya.kato@linecorp.com>
2025-01-07 12:12:53 +0900

    Debug: simple onRequestGet

--- functions/_worker.ts
+++ functions/_worker.ts
@@ -1,19 +1,6 @@
 /// <reference types="@cloudflare/workers-types" />
 
-function escapeHTML(str: string): string {
-  return str.replace(/[&<>"']/g, (match) => {
-    const escapeMap: Record<string, string> = {
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
-      "'": '&#039;',
-    };
-    return escapeMap[match] || match;
-  });
-}
-
-export const onRequest: PagesFunction = async (context) => {
+export const onRequestGet: PagesFunction = async (context) => {
   console.log("hello debug");
 
   const { request, next } = context;
@@ -25,22 +12,8 @@ export const onRequest: PagesFunction = async (context) => {
     return response;
   }
 
-  const url = new URL(request.url);
-  const title = url.searchParams.get("title") || "キノコ伝説ビルドシミュレーター";
-  const date = new Date().toISOString().split('T')[0];
-
   const html = await response.text();
-  console.log(escapeHTML(title));
-  const modifiedHtml = html.replace(
-    /<meta property="og:title" content=".*?">/,
-    `<meta property="og:title" content="${escapeHTML(title)}">`
-  ).replace(
-    /<meta property="og:image" content=".*?">/,
-    `<meta property="og:image" content="https://ogp-kino-simu.marumaru-niconico.workers.dev/?title=${encodeURIComponent(escapeHTML(title))}&date=${date}">`
-  );
-  console.log(modifiedHtml);
-
-  return new Response(modifiedHtml, {
+  return new Response(html, {
     status: response.status,
     headers: {
       "Content-Type": "text/html",
