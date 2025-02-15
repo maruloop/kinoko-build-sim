bf78b486aadb22c52b9c2068cf19104475b336fd
Toshiya-Kato <toshiya.kato@linecorp.com>
2025-02-15 17:34:37 +0900

    Rewrite OG Description

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,4 +1,5 @@
 /// <reference types="@cloudflare/workers-types" />
+import LZString from 'lz-string';
 
 function escapeHTML(str: string): string {
   return str.replace(/[&<>"']/g, (match) => {
@@ -25,6 +26,7 @@ export const onRequest: PagesFunction = async (context) => {
 
   const url = new URL(request.url);
   const title = url.searchParams.get("title") || "(非公式)キノコ伝説ビルドシミュレーター";
+  const description = LZString.decompressFromEncodedURIComponent(url.searchParams.get("description") || '') || "ビルドを作って共有しよう！"
   const date = new Date().toISOString().split('T')[0];
 
   const html = await response.text();
@@ -32,11 +34,17 @@ export const onRequest: PagesFunction = async (context) => {
     /<meta property="og:title" content=".*?">/,
     `<meta property="og:title" content="${escapeHTML(title)} - (非公式)キノコ伝説ビルドシミュレーター">`
   ).replace(
+    /<meta property="og:description" content=".*?">/,
+    `<meta property="og:description" content="${escapeHTML(description)}">`
+  ).replace(
     /<meta property="og:image" content=".*?">/,
     `<meta property="og:image" content="https://ogp-kino-simu.marumaru-niconico.workers.dev/?title=${encodeURIComponent(escapeHTML(title))}&date=${date}">`
   ).replace(
     /<meta property="twitter:title" content=".*?">/,
     `<meta property="twitter:title" content="${escapeHTML(title)} - (非公式)キノコ伝説ビルドシミュレーター">`
+  ).replace(
+    /<meta property="twitter:description" content=".*?">/,
+    `<meta property="twitter:description" content="${escapeHTML(description)}">`
   ).replace(
     /<meta property="twitter:image" content=".*?">/,
     `<meta property="twitter:image" content="https://ogp-kino-simu.marumaru-niconico.workers.dev/?title=${encodeURIComponent(escapeHTML(title))}&date=${date}">`
